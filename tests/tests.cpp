e7da8a00c9757e70e756e1a2375bf0df7d23ded5
Adam Nielsen <malvineous@shikadi.net>
2015-03-17 23:32:38 +1000

    Tests: Correctly colour output if mismatch is on first line

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -43,14 +43,16 @@ void default_sample::printNice(boost::test_tools::predicate_result& res,
 	const char *c = CLR_YELLOW;
 	res.message() << c;
 	std::ostringstream text;
+	text << c;
 	unsigned int len = s.length();
 	for (unsigned int i = 0; i < len; i++) {
 		if ((i > 0) && (i % this->outputWidth == 0)) {
-			res.message() << ' ' << CLR_YELLOW << text.str();
+			res.message() << ' ' << text.str();
 			res.message() << CLR_NORM << "\n" << std::setfill('0') << std::setw(3)
 				<< std::hex << i << ": " << c;
 			text.str("");
 			text.seekp(0, std::ios::beg);
+			res.message() << c;
 			text << c;
 		}
 		if ((i >= diff.length()) || (s[i] != diff[i])) {
