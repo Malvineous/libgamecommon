22454eee1e04a6e25721664fd998b78400e20199
Adam Nielsen <malvineous@shikadi.net>
2016-04-22 18:23:16 +1000

    Create m4 dir in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@
 # Remove M4 files automatically copied (in case autotools version changes)
 rm -f m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4
 
+mkdir -p m4
 autoreconf -fi || exit $?
 
 rm -Rf autom4te*.cache
