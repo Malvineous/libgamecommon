5df440f962688a01636a19e948972fa27f95bffb
Adam Nielsen <malvineous@shikadi.net>
2017-10-02 18:56:34 +1000

    tests: Use header-only Boost.Test (but still from system-wide install)
    This avoids the need to link with anything.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -19,8 +19,7 @@
  */
 
 #define BOOST_TEST_MODULE libgamecommon
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 #include <boost/algorithm/string.hpp> // for case-insensitive string compare
 #include <iostream>
